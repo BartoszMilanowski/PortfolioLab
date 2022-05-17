6c668a3b2e0184262a777089242105310c5907a0
BartoszMilanowski <bartosz.milanowski@protonmail.com>
2022-05-17 15:34:09 +0200

    Create file data.sql with sample inserts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into charity_donation.categories (name) values ('ubrania');
+insert into charity_donation.categories (name) values ('zabawki');
+insert into charity_donation.categories (name) values ('dania instant');
+insert into charity_donation.categories (name) values ('makarony');
+
+insert into charity_donation.institutions (name, description) values ('Fundacja "Bez domu"', 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania');
