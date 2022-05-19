48a43fa4d9a7ed6e57f83ecc4aae6048fb5203eb
BartoszMilanowski <bartosz.milanowski@protonmail.com>
2022-05-19 18:29:15 +0200

    Create entity User with repo and service

---
+++ src/main/java/pl/coderslab/charity/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package pl.coderslab.charity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import pl.coderslab.charity.entity.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select u from User u where u = ?1")
+    User findByOpt(Optional<User> userOpt);
+
+    User findByEmail(String email);
+
+}
